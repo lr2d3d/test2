56b8ecc31b3ac646bec500fe008093d3f03a217c
lr2d3d <56827869+lr2d3d@users.noreply.github.com>
2019-11-22 14:08:06 +0100

    8

--- testgit2/ViewController.swift
+++ testgit2/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         khqsdlkgqhgqgjqlgjdkql
         kjhkhkkjhkj
         kjhkhkhkhkjh
+        lkjdslmfgjqljglqj
+        mjmqjsdklgjqldgs
         // Do any additional setup after loading the view.
         
         mljjljjkl
