45706e987c113e31a43e6ee1a5b322e507d8eef9
lr2d3d <56827869+lr2d3d@users.noreply.github.com>
2019-11-22 14:06:59 +0100

    dogged

--- testgit2/ViewController.swift
+++ testgit2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         sdlkhgfqshgdlsd
         lkjsdhlgqqjsldgjqlsdg
         mldfklmgkmgqksgs
+        khqsdlkgqhgqgjqlgjdkql
         // Do any additional setup after loading the view.
         
         mljjljjkl
